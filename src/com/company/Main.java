c7b0961c67f0228e0b975ea9e47784948686bc0e
Kymbrlee <kymba7@gmail.com>
2018-01-22 16:18:52 -0700

    Day10Animal

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Animal panda = new Animal("coco");
+        panda.speak();
+        Dog lab= new Dog("Max");
+        lab.speak();
+        Cat tabby = new Cat("Tweetie");
+        tabby.speak();
+    }
+}
